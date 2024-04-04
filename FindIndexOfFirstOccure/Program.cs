be11ce2a679b7b94b67e30cef4e3452f5666b96c
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-04-04 05:25:46 +0500

    FindofFirstOccure

--- FindIndexOfFirstOccure/Program.cs
+++ FindIndexOfFirstOccure/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var s = new Solution();
+
+Console.WriteLine(s.StrStr("sadbutsad", "sad"));
+
+public class Solution
+{
+    public int StrStr(string haystack,string needle)
+    {
+        for(int i=0;i<haystack.Length-needle.Length+1;i++)
+        {
+            for(int j=1;j<needle.Length+1;j++)
+            {
+                if(haystack.Substring(i,j)!=needle.Substring(0,j))
+                {
+                    break;
+                }
+                if(haystack.Substring(i,j)==needle)
+                {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}

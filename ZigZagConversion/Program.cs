b88d8593f118aa825e8f0c1c7704b3d2b47bfa28
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-04-05 17:52:07 +0500

    ZigZag Conversion

---
+++ ZigZagConversion/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+using System.Text;
+
+public class Solution
+{
+    public string Convert(string s, int numRows)
+    {
+        if(numRows==1)
+        {
+            return s;
+        }
+        StringBuilder[] asArr= new StringBuilder[numRows];
+        int r= 0;
+        bool goingD = true;
+        foreach (var item in s)
+        {
+            asArr[r].Append(item);
+            if(r==0)
+            {
+                goingD = true;
+
+            }
+            else if (r+1== numRows)
+            {
+                goingD= false;
+            }
+            r=goingD ? r+1 : r-1;
+
+        }
+
+        string ziGZag = "";
+        foreach (var item in asArr)
+        {
+            ziGZag += item.ToString();
+        }
+        return ziGZag;
+    }
+}

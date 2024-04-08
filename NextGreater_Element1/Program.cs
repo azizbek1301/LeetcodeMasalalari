c72a7b58de0142854788811f61143b991b6caa3b
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-04-08 23:04:08 +0500

    Next Greater_Element1

---
+++ NextGreater_Element1/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int FindNextGreaterElement(int ele, int[] arr)
+    {
+        int idx = Array.IndexOf(arr, ele);
+        int ans = -1;
+        for (int i = idx + 1; i < arr.Length; i++)
+        {
+            if (arr[i] > ele)
+            {
+                ans = arr[i];
+                break;
+            }
+        }
+        return ans;
+    }
+    public int[] NextGreaterElement(int[] nums1, int[] nums2)
+    {
+        int[]? ans = new int[nums1.Length];
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            ans[i] = FindNextGreaterElement(nums1[i], nums2);
+        }
+        return ans;
+    }
+}

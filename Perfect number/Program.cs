ae8e68a2a2e5bd237da7007cabe48bbfcecd52a5
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-04-09 22:15:50 +0500

    perfect number

---
+++ Perfect number/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+public class Solution
+{
+    public bool CheckPerfectNumber(int num)
+    {
+
+        int sum = 0;
+        for(int i=1;i<=num/2;i++)
+        {
+            if(num%i==0)
+            {
+                sum = sum + i;
+            }
+        }
+        if(sum==num)
+        {
+            return true;
+        }
+        return false;
+    }
+}

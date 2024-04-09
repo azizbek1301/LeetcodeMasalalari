1d574cc46ab87885861c0984e324f74577dd4c25
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-04-09 22:20:58 +0500

    Detect_Words

---
+++ Detect_Words/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Solution
+{
+    public bool DetectCapitalUse(string word)
+    {
+        int counter = 0;
+        foreach (char c in word)
+        {
+            if (c >= 65 && c <= 90) counter++;
+        }
+        if (counter == 0 || counter == word.Length || (counter == 1 && (word[0] >= 65 && word[0] <= 90))) return true;
+        return false;
+    }
+}

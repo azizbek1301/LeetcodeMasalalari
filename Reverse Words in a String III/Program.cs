e28346d3e368fe7b4f6fa3a03c6a5a617c29b1c8
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-04-29 22:32:39 +0500

    Reverse words in String |||

---
+++ Reverse Words in a String III/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.Text;
+
+public class Solution
+{
+    public string ReverseWords(string s)
+    {
+        string[]? arrayOfWords = s.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        StringBuilder reversStringBuilder= new StringBuilder("");
+
+        foreach (var item in arrayOfWords)
+        {
+            char[] chArr= item.ToCharArray();
+            Array.Reverse(chArr);
+            reversStringBuilder.Append(" "+new string(chArr));
+        }
+        string result= reversStringBuilder.ToString().Substring(1);
+        return result;
+    }
+}

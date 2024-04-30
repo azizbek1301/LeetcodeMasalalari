f89f468505ad89fbd4d0dae4375a56764419e6de
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-04-30 23:04:11 +0500

    Reverse string ||

---
+++ ReverseString 2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+Solution sol = new Solution();
+
+var a=sol.ReverseStr("asilhre", 2);
+Console.WriteLine(a);
+
+public class Solution
+{
+    public string ReverseStr(string s, int k)
+    {
+        if(k<0 ||  k>s.Length)
+        {
+            return null;
+        }
+        string str = string.Empty;
+        for (int i = k; i >=0; i--)
+        {
+            str+= s[i];
+        }
+        for (int i = k; i <s.Length;i++)
+        {
+            str+= s[i];
+        }
+
+        return str;
+    }
+}

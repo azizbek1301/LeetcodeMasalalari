2a6a8d1dd97683a4f40a5f655576b4faba4c5aaa
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-04-30 22:59:46 +0500

    Reverse Integer

---
+++ Reverse Integer/Program.cs
@@ -0,0 +1,39 @@
+﻿
+var sol =new  Solution();
+
+Console.WriteLine( sol.Reverse(1534236469));
+
+public class Solution
+{
+    public int Reverse(int x)
+    {
+        string newSon = string.Empty;
+        int chekManfiy = (x < 0) ? -1 : 1;
+        try
+        {
+            checked
+            {
+                
+                int castedNumber = (int)x;
+                if (castedNumber < 10 && castedNumber > -1)
+                {
+                    return castedNumber;
+                }
+                castedNumber = Math.Abs(castedNumber);
+                while (castedNumber > 9)
+                {
+                    newSon += (castedNumber % 10).ToString();
+                    castedNumber = castedNumber / 10;
+                }
+                newSon += castedNumber.ToString();
+                return int.Parse(newSon) * chekManfiy;
+
+                
+            }
+        }
+        catch (OverflowException)
+        {
+            return 0;
+        }
+    }
+}

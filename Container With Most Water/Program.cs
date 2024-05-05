4f0c7408b135e124afb6d3a7bc78a667e945205d
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-05-05 16:09:25 +0500

    Container with most warter

---
+++ Container With Most Water/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        int left = 0;
+        int right = height.Length - 1;
+        int maxWater = 0;
+
+        while (left < right)
+        {
+            int area = Math.Min(height[left], height[right]) * (right - left);
+            maxWater = Math.Max(maxWater, area);
+
+            if (height[left] < height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+}

9d8d54085c694bae98d0382d32246b0b576094dd
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-05-06 22:40:21 +0500

    int to roman

--- Integer to roman/Program.cs
+++ Integer to roman/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+public class Solution
+{
+    public string IntToRoman(int num)
+    {
+        var romanNumbers = new Dictionary<int, string>()
+        {
+            { 1000, "M" },
+            { 900, "CM" },
+            { 500, "D" },
+            { 400, "CD" },
+            { 100, "C" },
+            { 90, "XC" },
+            { 50, "L" },
+            { 40, "XL" },
+            { 10, "X" },
+            { 9, "IX" },
+            { 5, "V" },
+            { 4, "IV" },
+            { 1, "I" }
+        };
+
+        string result=string.Empty;
+
+        foreach (var item in romanNumbers.OrderByDescending(x=>x.Key))
+        {
+            while(num>=item.Key)
+            {
+                result += item.Value;
+                num-=item.Key;
+            }
+        }
+        return result;
+    }
+}
